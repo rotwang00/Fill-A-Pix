50f419439d4a51fb2242d67767a05178a8b83f1f
Paul Thurmond <paulthurmond@gmail.com>
2021-07-06 10:08:04 -0500

    Deleted old stuff

--- app.js
+++ app.js
@@ -1,35 +1,7 @@
-let numberLit = 25;
-const displayScore = document.getElementById("score");
-let size = 5;
 let cellStatus = Array.from(Array(size), () => new Array(size));
 
 function processClick() {
     toggleState(this);
-    const clickedSquareID = this.id;
-    const x = parseInt(clickedSquareID[1]);
-    const y = parseInt(clickedSquareID[0]);
-    console.log(`Y = ${y}, X = ${x}`);
-    if (x > 0) {
-        const newID = `${y}${x - 1}`;
-        const sq = document.getElementById(newID);
-        toggleState(sq);
-    }
-    if (x < 4) {
-        const newID = `${y}${x + 1}`;
-        const sq = document.getElementById(newID);
-        toggleState(sq);
-    }
-    if (y > 0) {
-        const newID = `${y - 1}${x}`;
-        const sq = document.getElementById(newID);
-        toggleState(sq);
-    }
-    if (y < 4) {
-        const newID = `${y + 1}${x}`;
-        const sq = document.getElementById(newID);
-        toggleState(sq);
-    }
-    console.log(cellStatus);
 }
 
 function toggleState(current) {
@@ -37,28 +9,13 @@ function toggleState(current) {
     const x = parseInt(current.id[1]);
     const y = parseInt(current.id[0]);
     const classes = current.classList;
-    if (classes.contains("lit")) {
+    if (classes.contains('lit')) {
         numberLit++;
     } else {
         numberLit--;
     }
-    current.classList.toggle("lit");
+    current.classList.toggle('lit');
     cellStatus[x][y] = 1 - cellStatus[x][y];
-    displayScore.innerHTML = `Number lit: ${numberLit}`;
-    if (numberLit === 0) {
-        win();
-    }
-}
-
-function win() {
-    const winMessage = document.createElement("p");
-    winMessage.innerHTML = "You win!";
-    score.appendChild(winMessage);
-    const resetButton = document.createElement("button");
-    resetButton.innerHTML = "Play again";
-    score.appendChild(resetButton);
-    resetButton.addEventListener("click", resetGame, false);
-    resetButton.setAttribute("id", "resetButton");
 }
 
 function createGrid() {
@@ -67,24 +24,24 @@ function createGrid() {
     // This grid will have all cells lit
 
     // Create grid div
-    const newGrid = document.createElement("div");
-    newGrid.setAttribute("id", "grid");
+    const newGrid = document.createElement('div');
+    newGrid.setAttribute('id', 'grid');
 
     for (let i = 0; i < size; i++) {
         // Create row div
-        const newRow = document.createElement("div");
-        newRow.setAttribute("id", `row${i}`);
+        const newRow = document.createElement('div');
+        newRow.setAttribute('id', `row${i}`);
 
         for (let j = 0; j < size; j++) {
             // Create square
-            const newSquare = document.createElement("button");
-            newSquare.setAttribute("id", `${i}${j}`);
+            const newSquare = document.createElement('button');
+            newSquare.setAttribute('id', `${i}${j}`);
 
             // Add class
-            newSquare.classList.add("gridButton");
+            newSquare.classList.add('gridButton');
 
             // Add event listener
-            newSquare.addEventListener("click", processClick, false);
+            newSquare.addEventListener('click', processClick, false);
 
             // Append square to row div
             newRow.appendChild(newSquare);
@@ -96,7 +53,7 @@ function createGrid() {
         newGrid.appendChild(newRow);
     }
     // Append grid div
-    const puzzle = document.getElementById("puzzle");
+    const puzzle = document.getElementById('puzzle');
     puzzle.appendChild(newGrid);
 }
 
